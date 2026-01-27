3bbb578bb9cafe990c15dedf0381568a65c04e6f
tunbudi06 <anjaytun2006@gmail.com>
2026-01-27 08:53:36 +0700

    adding svelte and thei config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,44 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+// import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/iseki_kyt/public/build',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks:{
+                    'svelte-vendor': ['svelte', 'svelte/animate', 'svelte/easing', 'svelte/motion', 'svelte/store', 'svelte/transition'],
+                    'icons-vendor': ['@lucide/svelte'],
+                },
+                chunkFileNames: (chunkInfo) => {
+                    if (chunkInfo.name === 'svelte-vendor') {
+                        return 'vendor/svelte-vendor.[hash].js';
+                    }
+                    if (chunkInfo.name === 'icons-vendor') {
+                        return 'vendor/icons-vendor.[hash].js';
+                    }
+                    return 'assets/[name].[hash].js';
+                }
+            }
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
+        svelte(),
         tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+    resolve: {
+        alias: {
+            '$': path.resolve(__dirname, './resources/js'),
+            '$shadcn': path.resolve(__dirname, './resources/js/shadcn'),
+            '$routes': path.resolve(__dirname, './resources/js/routes')
         },
     },
 });
