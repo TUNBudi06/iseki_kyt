cba95fa54befa078728654c865afb9de2a56b411
tunbudi06 <anjaytun2006@gmail.com>
2026-06-29 15:52:45 +0700

    fix: eager import.meta.glob in app.js resolve
    - Move import.meta.glob() outside resolve (evaluated once)
    - Add { eager: true } so modules are pre-loaded, not factory functions
    - Add error logging if a page is not found

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,16 @@ import { createApp, h } from 'vue'
 import './bootstrap'
 import { initRouteHelper } from '@tunbudi06/inertia-route-helper/init'
 
+const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.vue')
-        return pages[`./Pages/${name}.vue`]()
+        const page = pages[`./Pages/${name}.vue`]
+        if (!page) {
+            console.error(`Page not found: ./Pages/${name}.vue`)
+            console.log('Available pages:', Object.keys(pages))
+        }
+        return page
     },
     setup({ el, App, props, plugin }) {
         initRouteHelper(props)
